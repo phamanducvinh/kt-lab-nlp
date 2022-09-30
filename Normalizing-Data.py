ec28284ff64c3a91c3471dad92d13cd4d8b34061
Virtu <ductrong405@gmail.com>
2022-09-30 21:27:38 +0700

    updated raw data

---
+++ Normalizing-Data.py
@@ -0,0 +1,16 @@
+from glob import glob
+import os.path
+import json
+
+for filepath in glob("original/*.json"):
+
+    with open(filepath, "r", encoding="utf8") as openfile:
+        json_object = json.load(openfile)
+
+    for line in range(len(json_object)):
+        json_object[line]["content"] = ' '.join(json_object[line]["content"])
+
+    json_modified = json.dumps(json_object);
+
+    with open("modified.txt", "w", encoding = 'utf8') as json_ModifiedFile:
+        json.dump(json_modified, json_ModifiedFile)
